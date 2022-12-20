0ecda19c8421148f8fcc91d217c74a09c6ca19da
SilverSunLine <100753335+EunHyeSeon@users.noreply.github.com>
2022-12-20 17:43:29 +0900

    Create 전화번호 목록.py

---
+++ Level-2/전화번호 목록.py
@@ -0,0 +1,14 @@
+def solution(phone_book):
+    answer = True
+    hash = {}
+    
+    for i in phone_book:
+        hash[i] = 1
+        
+    for i in phone_book:
+        temp = ''
+        for j in i:
+            temp += j
+            if temp in hash and temp != i:
+                return False
+    return answer
