512d29d9da373d5f48bf3e3df957c8855ccb1b77
Hye_ <100753335+thecodeiswell@users.noreply.github.com>
2022-12-31 15:37:26 +0900

    Create 디스크-컨트롤러.py

---
+++ Level-3/디스크-컨트롤러.py
@@ -0,0 +1,23 @@
+import heapq
+
+def solution(jobs): 
+    answer, now, i = 0, 0, 0
+    start = -1
+    heap = []
+    
+    while i < len(jobs):
+        # 현재 시점에서 처리할 수 있는 작업들 저장
+        for j in jobs:
+            if start < j[0] <= now:
+                heapq.heappush(heap, (j[1], j[0]))
+        
+        if heap:
+            cur = heapq.heappop(heap)
+            start = now
+            now += cur[0]
+            answer += now - cur[1]
+            i += 1
+        else:
+            now += 1
+            
+    return answer // i
