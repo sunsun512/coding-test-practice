4a9f0841806ac184e4439cc19007fc75e14f0db8
Hye_ <100753335+thecodeiswell@users.noreply.github.com>
2023-01-14 21:23:33 +0900

    Update 문자열-정렬하기-(2).py

--- Level-0/문자열-정렬하기-(2).py
+++ Level-0/문자열-정렬하기-(2).py
@@ -1,2 +1,7 @@
 def solution(my_string):
     return ''.join(sorted(my_string.lower()))
+
+#javascript solution
+#function solution(my_string) {
+#    return my_string.toLowerCase().split('').sort().join('');
+#}
