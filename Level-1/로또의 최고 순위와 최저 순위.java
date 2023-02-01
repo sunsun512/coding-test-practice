b8a45ca2f7041dfe6edbf09202667c6ad70c6a82
Hye_ <100753335+thecodeiswell@users.noreply.github.com>
2023-02-01 21:51:27 +0900

    Update 로또의 최고 순위와 최저 순위.java

--- Level-1/로또의 최고 순위와 최저 순위.java
+++ Level-1/로또의 최고 순위와 최저 순위.java
@@ -17,3 +17,37 @@ class Solution {
         return new int[]{7-Math.max(cnt+zeroCnt,1),7-Math.max(cnt,1)};
     }
 }
+
+
+// 고수의 
+
+// import java.util.HashMap;
+// import java.util.Map;
+
+// class Solution {
+//     public int[] solution(int[] lottos, int[] win_nums) {
+//         Map<Integer, Boolean> map = new HashMap<Integer, Boolean>();
+//         int zeroCount = 0;
+
+//         for(int lotto : lottos) {
+//             if(lotto == 0) {
+//                 zeroCount++;
+//                 continue;
+//             }
+//             map.put(lotto, true);
+//         }
+
+
+//         int sameCount = 0;
+//         for(int winNum : win_nums) {
+//             if(map.containsKey(winNum)) sameCount++;
+//         }
+
+//         int maxRank = 7 - (sameCount + zeroCount);
+//         int minRank = 7 - sameCount;
+//         if(maxRank > 6) maxRank = 6;
+//         if(minRank > 6) minRank = 6;
+
+//         return new int[] {maxRank, minRank};
+//     }
+// }
