87669e0b4e712d924addafbfdbf439661e91099b
Hye_ <100753335+thecodeiswell@users.noreply.github.com>
2023-02-16 18:18:08 +0900

    Create 택배-배달과-수거하기.py

---
+++ Level-2/택배-배달과-수거하기.py
@@ -0,0 +1,18 @@
+def solution(cap, n, deliveries, pickups):
+    deliveries = deliveries[::-1]
+    pickups = pickups[::-1]
+    answer = 0
+
+    deli = 0
+    pick = 0
+
+    for i in range(n):
+        deli += deliveries[i]
+        pick += pickups[i]
+
+        while deli > 0 or pick > 0:
+            deli -= cap
+            pick -= cap
+            answer += (n - i) * 2
+
+    return answer
